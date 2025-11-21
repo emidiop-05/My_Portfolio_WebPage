b3b94ffbe9d896f74e9edb60be860b13e0ca8f4b
Emidio Pedro <emidiop44@gmail.com>
2025-11-21 15:19:35 +0100

    navbar

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Navbar({ toggleSidebar }) {
+  return <h1>Hello world!</h1>;
+}
+
+export default Navbar;
