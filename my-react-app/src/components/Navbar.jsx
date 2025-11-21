70fef6ecff2685b6efcb2bbbbcfb46b5e6a258ac
Emidio Pedro <emidiop44@gmail.com>
2025-11-21 22:29:51 +0100

    navbar advances

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,8 +1,39 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
-function Navbar({ toggleSidebar }) {
-  return <h1>Hello world!</h1>;
+import LogoIcon from "../assets/Logo.svg?react";
+
+function Navbar() {
+  return (
+    <nav className={styles.Navbar}>
+      <Link to="/" className={styles.logoContainer}>
+        <LogoIcon className={styles.logoSvg} />
+      </Link>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <a href="#about-me" className={styles.navLink}>
+            About me
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#experience" className={styles.navLink}>
+            Experience
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#projects" className={styles.navLink}>
+            Projects
+          </a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#contact-info" className={styles.navLink}>
+            Contact info
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
